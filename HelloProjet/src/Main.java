c15c986d158a54ff2552291e6c81c86105a59e0f
15735105176 <505606242@qq.com>
2019-08-06 22:05:15 +0800

    xiu

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,8 +6,6 @@ public class Main {
 public void testHelloWrold() {
 System.out.println("1232132131231");
 System.out.println("HelloWorld!");
-<<<<<<< HEAD
 System.out.println("³åÍ»111111111");}}
-=======
 System.out.println("³åÍ»ÁË");}}
->>>>>>> 60c82f26870ac3b74a49de3a6db41d9eed35e65b
+
