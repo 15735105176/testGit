0f985cde0aab7316613dd47d49ed09d56561aac7
15735105176 <505606242@qq.com>
2019-08-06 21:43:01 +0800

    rm仓库提交demo

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("1232132131231");
         System.out.println("HelloWorld!");
-		System.out.println("00000000000000!");
+		System.out.println("rm仓库提交demo");
     }
 }
