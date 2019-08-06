16ba22330b37e93f496f46990a7c33398e612097
15735105176 <505606242@qq.com>
2019-08-06 22:08:25 +0800

    4324

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,5 @@ public void testHelloWrold() {
 System.out.println("1232132131231");
 System.out.println("HelloWorld!");
 System.out.println("³åÍ»111111111");}}
-System.out.println("³åÍ»ÁË");}}
+System.out.println("³åÍ»ÁË");}}1111111111111111
 
