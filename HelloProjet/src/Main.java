7f6ecd176a111637d055c57747bb179f7a6b6cd2
15735105176 <505606242@qq.com>
2019-08-06 21:56:18 +0800

    wo 也提价demo

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,4 +6,4 @@ public class Main {
 public void testHelloWrold() {
 System.out.println("1232132131231");
 System.out.println("HelloWorld!");
-System.out.println("³åÍ»½â¾öÁË");}}
+System.out.println("³åÍ»111111111");}}
